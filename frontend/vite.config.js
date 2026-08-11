43c8948ded305b4873f24f752638975790907d25
Pranavvmore12 <141485313+Pranavvmore12@users.noreply.github.com>
2026-08-11 22:11:29 +0530

    handled security

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import dotenv from 'dotenv'
+dotenv.config()
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.BACKEND_API_URL,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
